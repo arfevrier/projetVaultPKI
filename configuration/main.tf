5a4929fd8dc30b403af90707e7dacf241e486dea
Arnaud <arrnicel@gmail.com>
2021-10-16 13:33:27 +0200

    Ajout de la config SSH pour l'ensembles des serveurs 4AS

---
+++ configuration/main.tf
@@ -0,0 +1,34 @@
+provider "vault" {
+    address = "https://192.168.100.3:8200/"
+    skip_tls_verify = true
+    # Token: export VAULT_TOKEN="xxxx"
+}
+
+resource "vault_mount" "ssh-4as" {
+    type = "ssh"
+    path = "ssh-4as"
+
+    # Accès pour 4h par défaut, jusqu'à 12h.
+    default_lease_ttl_seconds = "14400"  # 4h
+    max_lease_ttl_seconds     = "43200" # 1w
+}
+
+resource "vault_ssh_secret_backend_ca" "ssh-4as-ca" {
+    backend = vault_mount.ssh-4as.path
+    generate_signing_key = true
+}
+
+resource "vault_ssh_secret_backend_role" "ssh-4as-admin" {
+    name     = "admin"
+    backend  = vault_mount.ssh-4as.path
+    key_type = "ca"
+    algorithm_signer = "rsa-sha2-256"
+
+    allow_user_certificates = true
+    default_user = "root"
+    allowed_users = "*"
+    default_extensions = {
+        permit-pty = ""
+        permit-port-forwarding = ""
+    }
+}
