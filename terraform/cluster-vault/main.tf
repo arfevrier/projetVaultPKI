c8d16a5813c240376910528c8713462c48b4fba6
Amadoubat19 <38119449+Amadoubat19@users.noreply.github.com>
2021-11-24 11:00:52 +0100

    Added allowed domains and subdomains for 4as

--- terraform/cluster-vault/main.tf
+++ terraform/cluster-vault/main.tf
@@ -157,6 +157,8 @@ resource "vault_ssh_secret_backend_role" "ssh-4as-host" {
     algorithm_signer = "rsa-sha2-256"
 
     allow_host_certificates = true
+    allowed_domains  = ["4as"]
+    allow_subdomains = true
 }
 
 resource "vault_policy" "ssh-4as-host-full-access" {
